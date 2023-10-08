8ed0486cfd0f4acf0e3f3de832226a7e97c5cfd8
吴南李 <wunanli2002@163.com>
2023-10-08 21:27:26 +0800

    通过语法分析辅助测试
    修改了文件输出方式

--- src/Compiler.java
+++ src/Compiler.java
@@ -8,7 +8,7 @@ public class Compiler {
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new FileReader("testfile.txt"));
-            FileWriter fw = new FileWriter("output.txt",true);
+            FileWriter fw = new FileWriter("output.txt",false);
             Lexer lexer = new Lexer(br);
             GrammarParser grammarParser = new GrammarParser(lexer.getSymbol());
             for (String s : grammarParser.getResult()) {

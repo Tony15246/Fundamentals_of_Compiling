1417eb83ce9bd63447edb447b3b906cfc53a7608
Tony15246 <wunanli2002@163.com>
2023-12-05 20:31:59 +0800

    synchronize

--- src/midend/value/user/IcmpUser.java
+++ src/midend/value/user/IcmpUser.java
@@ -1,4 +1,22 @@
 package midend.value.user;
 
+import midend.value.Value;
+
 public class IcmpUser extends User{
+    Value outputValue;
+    Value leftValue;
+    Value rightValue;
+    String cond;
+
+    public IcmpUser(Value outputValue, Value leftValue, Value rightValue, String cond) {
+        this.outputValue = outputValue;
+        this.leftValue = leftValue;
+        this.rightValue = rightValue;
+        this.cond = cond;
+    }
+
+    @Override
+    public String toString() {
+        return outputValue.toString() + " = icmp " + cond + " i32 " + leftValue.toString() + ", " + rightValue.toString();
+    }
 }
